f9830c505e3c9994ec9ddbeb5b53b8eb44cabd36
Katypires <katianepiresrc@gmail.com>
2024-06-21 17:02:47 -0400

    Atualizado

--- app/Http/Livewire/Admin/Sesau/Juridico/JuridicoPdfComponent.php
+++ app/Http/Livewire/Admin/Sesau/Juridico/JuridicoPdfComponent.php
@@ -9,5 +9,13 @@ class JuridicoPdfComponent extends Component
     public function render()
     {
         return view('livewire.admin.sesau.juridico.juridico-pdf-component', ['sindicancias'=>Sindicancia::all()]);
+
+
     }
+
+    // public function pdf($id) {
+    //     $sindicancia = Sindicancia::where('id',$id);
+    //     dd($sindicancia);
+    //     return view('livewire.admin.sesau.juridico.juridico-pdf-component', ['sindicancias'=>Sindicancia::where('id',$id)]);
+    // }
 }
--- resources/views/livewire/admin/sesau/Juridico/sindicancia/table/action.blade.php
+++ resources/views/livewire/admin/sesau/Juridico/sindicancia/table/action.blade.php
@@ -1,4 +1,5 @@
 <button  class="btn btn-success" data-bs-toggle="modal" data-bs-target="#Oitiva"  wire:click="$emit('openModalCrudTab', {{$model}})">Oitiva</button>
 <button  class="btn btn-success" data-bs-toggle="modal" data-bs-target="#SindicanciaMembro"  wire:click="$emit('openModalCrudTab', {{$model}})">Membros</button>
 <button  class="btn btn-success" data-bs-toggle="modal" data-bs-target="#SindicanciaPessoa"  wire:click="$emit('openModalCrudTab', {{$model}})">Pessoas</button>
-<button  class="btn btn-success"  wire:click="pdf">PDF</button>
+<button  class="btn btn-success"  wire:click="pdf">gerar PDF</button>
+{{-- <button  class="btn btn-success"  wire:click="pdf({{ $data['id'] }})">gerar PDF</button> --}}
